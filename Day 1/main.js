2b04603a47f6087a5953ebf42f9e6c41e5bd8d4e
Ekramz <83351624+Ekramz@users.noreply.github.com>
2021-09-02 22:01:57 +0600

    Add files via upload

---
+++ Day 1/main.js
@@ -0,0 +1,19 @@
+const panel = document.querySelectorAll('.panel')
+panel.forEach((panel)=>{
+   panel.addEventListener('click',()=>{
+       remove()
+       panel.classList.add('active')
+   })
+})
+
+function remove(){
+    panel.forEach((panel)=>{
+       
+         
+            panel.classList.remove('active')
+       
+     })
+
+
+
+}
